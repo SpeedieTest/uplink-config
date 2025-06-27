1f82ebc967537be771b023d60115fc4954aa9830
SpeedieTest <speediestester@gmail.com>
2025-06-28 06:49:04 +1000

    Update init.js

--- init.js
+++ init.js
@@ -3,3 +3,7 @@ var beacon = {
   fallback_dns: "uplink-fallback.virelia-water.it.com",
   token: "JBSWY3DPEBLW64TMMQQQ=="
 };
+
+if (beacon.token === "JBSWY3DPEBLW64TMMQQQ==") {
+  document.querySelector('.warning').innerText = "THM{this_is_the_real_flag}";
+}
